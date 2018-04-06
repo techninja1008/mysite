6d5a196185f85be95ea98c4a398587427e9c94c2
lukespademan <info@lukespademan.com>
2018-04-06 18:39:28 +0100

    fixed 404 msgs

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 app_name = 'home'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    re_path(r'^$', views.IndexView.as_view(), name='index'),
 ]
 
